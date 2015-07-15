59d8a290d3f1a29e62bdf4d1089d43803195b702
Vrael1993 <fheinze93@googlemail.com>
2015-07-15 11:22:44 +0200

    box health/pick

---
+++ Assets/boxHealth.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class boxHealth : MonoBehaviour {
+	public float startHealth = 100;
+	private float health = 100;
+	public Text healthText;
+
+	private bool damageable = true;
+	private bool isDead = false;
+	// Use this for initialization
+	void Start(){
+	
+		health = startHealth;
+
+	}
+
+	void Update () {
+		// FIXME: remove debug text
+		//healthText.text = health+" / "+ startHealth;
+	}
+	
+	void ApplyDamage(float damage){
+		if (damageable) {
+			health -= damage;
+			Debug.Log ("GetDamage");
+			health = Mathf.Max (0, health);
+			if (!isDead) {
+				if (health == 0) {
+					isDead = true;
+					GameObject.FindGameObjectWithTag("Player").GetComponent<FPController>().enabled = false;
+					//GameOver
+					Invoke ("GameOver", 1);
+				} 
+				damageable = false;
+				Invoke ("ResetDamageable", 1);
+			}
+		}
+	}
+
+	void GameOver(){
+		Application.LoadLevel (0);
+	}
+
+	void ResetDamageable(){
+		damageable = true;
+	}	
+}
---
+++ Assets/boxPick.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class boxPick : MonoBehaviour {
+	public GameObject kiste;
+	public GameObject kisteOnGround;
+	bool isPick = false;
+	// Use this for initialization
+	void Start () {
+		kiste.SetActive (false);
+		kisteOnGround.SetActive (true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.F)) {
+			if (isPick) {
+				kiste.SetActive (false);
+				kisteOnGround.SetActive (true);
+				kisteOnGround.transform.position = this.transform.position+this.transform.forward;
+				isPick = false;
+			} else {
+				RaycastHit hit = new RaycastHit (); 
+				Vector3 fwd = transform.forward;
+				Debug.DrawRay (transform.position, fwd);
+				if (Physics.Raycast (new Ray (this.transform.position, fwd), out hit, 2.0f) && hit.collider.CompareTag ("Kiste")) {
+					Debug.Log ("pickBox");
+					pick ();
+				}
+			}
+		}
+	}
+
+	void pick(){
+		if (isPick == false) {
+			//falls die Box nicht kindobjekt von Fp-Controller benötigt
+			//kiste.transform.position = this.transform.position+this.transform.forward;
+			kiste.SetActive (true);
+			kisteOnGround.SetActive (false);
+			isPick = true;
+		} 
+	}
+}
