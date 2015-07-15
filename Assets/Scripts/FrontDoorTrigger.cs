0a9ee6ce25be13ee4efa1a5390bb1f186f0e6c71
David-Neubauer <D.Neubauer@hochschule-trier.de>
2015-07-15 17:29:30 +0200

    FrontDoorTrigger added

---
+++ Assets/Scripts/FrontDoorTrigger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    internal class FrontDoorTrigger : MonoBehaviour
+    {
+        public bool IsBoxWithinCollider { get; private set; }
+
+        private void OnEnable()
+        {
+            IsBoxWithinCollider = false;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.gameObject.tag.Equals("Kiste")) return;
+            IsBoxWithinCollider = true;
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (!other.gameObject.tag.Equals("Kiste")) return;
+            IsBoxWithinCollider = false;
+        }
+    }
+}
