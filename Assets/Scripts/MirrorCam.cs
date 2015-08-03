da2f35c6c046d2219a83a7c4848c044cd624bad6
Oliver Biwer <soraphis@gmail.com>
2015-08-03 21:48:54 +0200

    physical correct mirror

---
+++ Assets/Scripts/MirrorCam.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MirrorCam : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 v = Camera.main.transform.position - transform.parent.position;
+		Vector3 r = 2 * Vector3.Dot(v, transform.parent.up)*transform.parent.up - v;
+		this.transform.LookAt(transform.parent.position + r);
+	}
+}
+
