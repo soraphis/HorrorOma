133c48af06b88354e3a900cb11b766cc8e875634
Oliver Biwer <soraphis@gmail.com>
2015-08-06 16:43:03 +0200

    electroraum und treppe

---
+++ Assets/dummy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class dummy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.B)){
+            Rigidbody rb = this.GetComponent<Rigidbody>();
+            rb.AddExplosionForce(300*Random.value*4, new Vector3(0.85f + Random.value, 0f, -1.95f - Random.value*3), 20+Random.value*6);
+        }
+	}
+}
