7fccec2b03f7fb69a4e1808f3b195d50a8538ee4
Vrael1993 <fheinze93@googlemail.com>
2015-08-27 14:43:43 +0200

    AmbientSound

---
+++ Assets/AmbientSound.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AmbientSound : MonoBehaviour {
+	//public AudioSource mainLoop;
+	public AudioSource track_1;
+	public AudioSource track_2;
+	public AudioSource track_3;
+
+	private bool playAudio = false;
+	private AudioClip[] clips;
+	private int i;
+	void Start(){
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	 	if (!playAudio) {
+			i = Random.Range (1, 3);
+			playSound();
+		}
+	}
+
+	void playSound(){
+	
+		StartCoroutine ("waitForRandomSeconds");
+	}
+
+	IEnumerator waitForRandomSeconds(){
+		yield return new WaitForSeconds(Random.Range(30,60));
+
+		if (i == 1) {
+			playAudio = true;
+			track_1.Play ();
+		}
+		if (i == 2) {
+			playAudio = true;
+			track_2.Play ();
+		}
+		if (i == 3) {
+			playAudio = true;
+			track_3.Play ();
+		} 
+
+	}
+}
---
+++ Assets/boxOutOfRegal.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class boxOutOfRegal : MonoBehaviour {
+	private Transform actor;
+	private bool throwAble;
+	// Use this for initialization
+	void Start () {
+		actor = GameObject.Find("Akteuer").transform;
+		throwAble = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		var dist = Vector3.Distance(actor.position, transform.position);
+		if (throwAble) {
+			if (dist < 10) {
+
+				Wurf ();
+				/*
+				Vector3 posAct = actor.position + new Vector3(1F,0F,0F);
+				transform.position = Vector3.MoveTowards (transform.position, posAct, 0.01F);
+				transform.LookAt(actor);
+				**/
+			}
+		}
+
+	}
+
+	void Wurf(){
+		Rigidbody myRigidbody = GetComponent<Rigidbody> ();
+		myRigidbody.AddForce(Vector3.forward * 2, ForceMode.Impulse);
+		throwAble = false;
+	}
+}
+
